c76f51695661039ec1b749dedca85544faa93ce0
VoskovschukDM <aamider@gmail.com>
2024-02-22 00:50:45 +0300

    booking limmit set 3

--- main.py
+++ main.py
@@ -3,7 +3,10 @@
 from bot import VkBot
 from langame_api import LangameApi
 
+
 langame = LangameApi()
 
 vk_bot = VkBot(langame)
 vk_bot.loop()
+
+#pyinstaller --noconfirm --onedir --console --name "booking_bot" --add-data "C:/Progs/booking_bot/config.txt;." --add-data "C:/Progs/booking_bot/PCs.txt;." --add-data "C:/Progs/booking_bot/rooms.txt;." --hidden-import "vkbottle" --hidden-import "selenium"  "C:/Progs/booking_bot/main.py"
